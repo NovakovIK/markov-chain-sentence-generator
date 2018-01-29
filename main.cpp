fe3fb6b1eea8ab0a197512f354fde2f2debfb342
Ivan Novakov <ivan.novakov.dev@gmail.com>
2018-01-29 22:05:06 +0200

    Add word-based "n-gram" probabilities from the source text

--- main.cpp
+++ main.cpp
@@ -35,17 +35,43 @@ std::vector<std::string> readWords(const std::string &path)
     }
 }
 
-std::string generateSentence(const std::string &path, size_t numberOfWords)
+std::string generateSentence(const std::string &path, size_t numberOfWords, size_t order)
 {
+    if (order < 2)
+    {
+        std::cerr << "Order must be more then 2.";
+        return std::string();
+    }
     auto words = readWords(path);
+    std::vector<std::string> ngrams;
+    for (auto i = words.begin(); i < words.end(); ++i)
+    {
+        std::string ngram;
+        for (int j = 0; j < order - 1; ++j)
+        {
+            ngram += *(i + j) + ' ';
+        }
+        ngrams.push_back(ngram);
+    }
+
     std::map<std::string, std::vector<std::string>> map;
 
-    for (auto i = words.begin(); i != words.end() - 1; ++i)
+    for (auto i = ngrams.begin(); i != ngrams.end() - 1; ++i)
     {
         map[*i].push_back(*(i + 1));
     }
 
-    std::string result = *randomSelect(words.begin(), words.end());
+
+    for (auto &i : ngrams)
+    {
+        std::cout << i << ':' << std::endl;
+        for (auto &j : map[i])
+        {
+            std::cout << "\t\t" << j << std::endl;
+        }
+    }
+
+    std::string result = *randomSelect(ngrams.begin(), ngrams.end());
 
     std::string current = result;
     std::string next;
@@ -54,7 +80,7 @@ std::string generateSentence(const std::string &path, size_t numberOfWords)
     for (int i = 0; i < numberOfWords; ++i)
     {
         next = *randomSelect(map[current].begin(), map[current].end());
-        result += ' ' + next;
+        result += next;
         current = next;
     }
 
@@ -63,5 +89,5 @@ std::string generateSentence(const std::string &path, size_t numberOfWords)
 
 int main()
 {
-    std::cout << generateSentence("text", 10) << std::endl;
+    std::cout << generateSentence("text", 10, 2) << std::endl;
 }
